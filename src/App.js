771d0cbfbb5c1c19b964a16cb8f00583907432ea
smathew32 <smathew32@myseneca.ca>
2024-07-12 11:51:30 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import Navbar from './components/Navbar'; // Navbar component
-import { BrowserRouter as Router, useRoutes} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './styles/Navbar.css'; 
+import './styles/Navbar.css'; // Ensure this path is correct
+
+// Components
+import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-
 // Pages
-import Homepage from './pages/homePage'; // Home page
+import Homepage from './pages/homePage';
 import AboutPage from './pages/aboutPage';
 import EventsCalendarPage from './pages/eventsCalendarPage';
 import GamesCollectionPage from './pages/gamesCollectionPage';
 import MenuPage from './pages/menuPage';
 import ReservationPage from './pages/reservationsPage';
 
-const AppRoutes = () => {
-    return useRoutes([
-        { path: "/", element: <Homepage /> },
-        { path: "/Events", element: <EventsCalendarPage /> },
-        { path: "/GameCollection", element: <GamesCollectionPage /> },
-        { path: "/Menu", element: <MenuPage /> },
-        { path: "/Reservation", element: <ReservationPage /> },
-        { path: "/about", element: <AboutPage /> },
-    ]);
-};
-
 const App = () => {
     return (
         <Router>
-            <Navbar />
-            <AppRoutes />
+            <Navbar /> 
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/Events" element={<EventsCalendarPage />} />
+                <Route path="/GameCollection" element={<GamesCollectionPage />} />
+                <Route path="/Menu" element={<MenuPage />} />
+                <Route path="/Reservation" element={<ReservationPage />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
             <Footer />
         </Router>
     );
