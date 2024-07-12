362415bb1d824e9ecf61ff5ecd54c38f24c7acc4
smathew32 <smathew32@myseneca.ca>
2024-07-12 00:07:04 -0400

    NavBar & Footer Complete?

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css';
+import instagramLogo from '../images/instagram.png';
+import facebookLogo from '../images/facebook.png';
+import discordLogo from '../images/discord.png';
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-light text-dark">
+            <div className="container py-3">
+                <div className="row">
+                    <div className="col-md-6">
+                        <address className="mb-0">
+                            471 5th Ave,<br />
+                            New York, 11215<br />
+                            <a href="mailto:sipnplaynyc@gamil.com">sipnplaynyc@gamil.com</a><br />
+                            718-971-1684
+                        </address>
+                    </div>
+                    <div className="col-md-6 text-md-end">
+                        <div className="company-name mb-2">Sip & Play</div>
+                        <div className="social-icons">
+                            <a href="https://www.instagram.com" className="me-2" aria-label="Instagram">
+                                <img src={instagramLogo} alt="Instagram" />
+                            </a>
+                            <a href="https://www.facebook.com" className="me-2" aria-label="Facebook">
+                                <img src={facebookLogo} alt="Facebook" />
+                            </a>
+                            <a href="https://www.discord.com" className="me-2" aria-label="Discord">
+                                <img src={discordLogo} alt="Instagram" /> 
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import logo from '../images/sipnplay.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './Navbar.css';
+
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="logo">
+        <nav className="navbar navbar-expand-lg custom-navbar">
+            <a className="navbar-brand" href="/">
+                <img src={logo} alt="My Logo" height="30" />
+            </a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/Events">Events</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/GameCollection">Game Collection</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/Menu">Menu</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/Reservation">Reservation</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about">About</a>
+                    </li>
+                </ul>
             </div>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/Events">Events</a></li>
-                <li><a href="/GameCollection">Game Collection</a></li>
-                <li><a href="/Menu">Menu</a></li>
-                <li><a href="/Reservation">Reservation</a></li>
-                <li><a href="/about">About</a></li>
-            </ul>
         </nav>
     );
 };
